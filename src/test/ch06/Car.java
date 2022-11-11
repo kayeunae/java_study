878ab0cbc3e5447cc6900b8674b3c9ac4c235f9b
kayeunae <verte_ai@naver.com>
2022-11-11 17:43:21 +0900

    up

---
+++ src/test/ch06/Car.java
@@ -0,0 +1,53 @@
+package test.ch06;
+
+public class Car {
+//	1. 필드: 객체의 데이터를 저장
+//	필드는 로컬 변수와는 다름(변수와 역할은 같지만)! 객체 내부에서 쓰거나 객체를 통해서만 쓸 수 있다.
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	int speed;
+//	boolean start;	//시동 여부
+
+	
+//	생성자 오버로드
+	Car(String model) {
+//		this.model = model;
+//		Car(String model, String color, int maxSpeed) 이 부분 호출
+		this(model, "은색", 250);
+	}
+	
+	Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+//		this(model, color, 250);
+	}
+	
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+//	java에서는 생성자를 명시하지 않으면 컴파일 과정에서 생성자를 생성해준다. 다만 빈 값의 형태로 !
+	Car(String model, String color, int maxSpeed, int speed) {
+		this.model = model;		//this.model: String model의 값 뒤의 model: new Car의 변수값
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+		this.speed = speed;
+	}
+	
+	/*
+	타입에 따른 초기값
+	 int, short, byte: 0
+	 long: 0L
+	 char: '\u0000'
+	 float: 0.0f
+	 double: 0.0
+	 boolean: false
+	 
+	 String: null
+	 */
+
+	}
---
+++ src/test/ch06/CarEx.java
@@ -0,0 +1,31 @@
+package test.ch06;
+
+public class CarEx {
+
+	public static void main(String[] args) {
+//		1. 객체 생성
+//		객체 외부에서 접근할 때는 myCar.model 와 같은 식으로 접근해야 함.
+//		객체가 생성될 때 객체가 실행된다. (Car의 매개 변수?에 아래의 값이 들어감)
+		Car myCar = new Car("그랜저", "검정", 250);
+		
+		System.out.println(myCar.company);
+		System.out.println(myCar.model);
+		System.out.println(myCar.color);
+		System.out.println(myCar.maxSpeed);
+		System.out.println(myCar.speed);
+
+		System.out.println("=====================");
+
+		Car car2 = new Car("자가용");
+		System.out.println(car2.model);
+		System.out.println(car2.color);
+		
+		System.out.println("=====================");
+
+		Car car3 = new Car("법인", "빨강");
+		System.out.println(car3.model);
+		System.out.println(car3.color);
+		
+	}
+
+}
