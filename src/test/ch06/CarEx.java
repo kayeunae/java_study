b59b68a322dbd7f5bc294e950a5cd4a052dd7cca
kayeunae <verte_ai@naver.com>
2022-11-11 17:49:23 +0900

    테스트(주석 추가)

--- src/test/ch06/CarEx.java
+++ src/test/ch06/CarEx.java
@@ -3,7 +3,7 @@ package test.ch06;
 public class CarEx {
 
 	public static void main(String[] args) {
-//		1. 객체 생성
+//		1. 객체 생성 
 //		객체 외부에서 접근할 때는 myCar.model 와 같은 식으로 접근해야 함.
 //		객체가 생성될 때 객체가 실행된다. (Car의 매개 변수?에 아래의 값이 들어감)
 		Car myCar = new Car("그랜저", "검정", 250);
@@ -25,6 +25,8 @@ public class CarEx {
 		Car car3 = new Car("법인", "빨강");
 		System.out.println(car3.model);
 		System.out.println(car3.color);
+		
+//		실행 순서를 이해해야 한다...~!
 		
 	}
 
